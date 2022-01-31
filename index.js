fb971c8073aa5f03987891ae0741b7aec3d1fe34
James Harrison <somtochukwujames2000@gmail.com>
2022-02-01 00:00:30 +0100

    Random Generator

---
+++ index.js
@@ -0,0 +1,12 @@
+const RandomFunction = (fixednumber) => {
+    let random = Math.floor(Math.random() * fixednumber);
+    return random;
+}
+
+console.log(RandomFunction(443));
+
+// The Function above generates random numbers, rounds it up to a whole number and return the value.
+
+// You can as well Modify it given Arrays of Data and you want to randomly select a Winner
+
+//WAGMI
